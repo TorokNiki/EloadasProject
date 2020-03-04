dde2b46dd1ffe5a672b1d84c7012c5ae7e6af4e1
Leora <55578239+TorokNiki@users.noreply.github.com>
2020-03-04 15:28:48 +0100

    Add files via upload
    Tesztesetek unit teszt

---
+++ Eloadas.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EloadasProject
+{
+    public class Eloadas
+    {
+        private bool[,] foglalasok;
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+
+            if (sorokSzama < 1 || helyekSzama < 1)
+            {
+
+                throw new ArgumentException("A megadott paramétereknek 0-nál nagyobbnak kell lenniük!!");
+            }
+            foglalasok = new bool[sorokSzama, helyekSzama];
+        }
+
+        public bool Lefoglal()
+        {
+            bool foglalt = false;
+
+            for (int i = 0; i < this.foglalasok.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < this.foglalasok.GetLength(1); j++)
+                {
+
+                    if (this.foglalasok[i, j] == false)
+                    {
+
+                        this.foglalasok[i, j] = true;
+                        foglalt = true;
+                        break;
+                    }
+                }
+                if (foglalt == true)
+                {
+                    break;
+                }
+            }
+            return foglalt;
+        }
+
+        public int SzabadHelyek
+        {
+            get
+            {
+                int szabadHely = 0;
+                for (int i = 0; i < this.foglalasok.GetLength(0); i++)
+                {
+
+                    for (int j = 0; j < this.foglalasok.GetLength(1); j++)
+                    {
+
+                        if (this.foglalasok[i, j] == false)
+                        {
+                            szabadHely++;
+                        }
+                    }
+                }
+                return szabadHely;
+            }
+        }
+
+        public bool Teli
+        {
+
+            get
+            {
+
+                return SzabadHelyek == 0;
+            }
+        }
+
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if (sorSzam < 1 || helySzam < 1)
+            {
+
+                throw new ArgumentException("A megadott paramétereknek 0-nál nagyobbnak kell lenniük!!");
+            }
+            return foglalasok[sorSzam - 1, helySzam - 1];
+        }
+    }
+}
+
---
+++ teszt/Teszt.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EloadasProject.teszt
+{
+    [TestFixture]
+    class Teszt
+    {
+        [TestCase]
+        public void szabadHelyek()
+        {
+            var test = new Eloadas(3, 3);
+            test.Lefoglal();
+            Assert.AreEqual(8, test.SzabadHelyek, "Hibás a férőhelyek száma");
+        }
+
+        [TestCase]
+        public void szabadHelyekTeli()
+        {
+            var test = new Eloadas(3, 3);
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            Assert.AreEqual(0, test.SzabadHelyek, "Hibás a férőhelyek száma");
+        }
+
+        [TestCase]
+        public void szabadHelyekMinusz()
+        {
+            var test = new Eloadas(2, 2);
+            Assert.Greater(test.SzabadHelyek, -1, "Hibás a férőhelyek száma");
+        }
+
+        [TestCase]
+        public void teliEloadas()
+        {
+            var test = new Eloadas(3, 3);
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            test.Lefoglal();
+            Assert.AreEqual(true, test.Teli, "Nem teli a Teli előadás.");
+        }
+
+        [TestCase]
+        public void nemTeliEloadas()
+        {
+            var test = new Eloadas(2, 2);
+            test.Lefoglal();
+            Assert.AreEqual(false, test.Teli, "Teli a nem teli előadás.");
+        }
+
+        [TestCase]
+        public void teliNull()
+        {
+            var test = new Eloadas(2, 2);
+            Assert.AreNotEqual(null, test.Teli, "A változó null.");
+        }
+
+
+        [TestCase]
+        public void foglaltTeszt()
+        {
+            var test = new Eloadas(3, 3);
+            test.Lefoglal();
+            Assert.AreEqual(true, test.Foglalt(1, 1), "Nem foglalt a felfoglalt hely.");
+        }
+
+        [TestCase]
+        public void foglaltNull()
+        {
+            var test = new Eloadas(3, 3);
+            test.Lefoglal();
+            Assert.AreNotEqual(null, test.Foglalt(1, 3), "A változó null.");
+        }
+
+        [TestCase]
+        public void lefoglalNull()
+        {
+            var test = new Eloadas(3, 3);
+            Assert.AreNotEqual(null, test.Lefoglal(), "A változó null.");
+        }
+
+        [TestCase]
+        public void helytelenBemenet()
+        {
+            Assert.Throws<ArgumentException>(() => {
+                var test = new Eloadas(0, -1);
+            });
+        }
+
+        [TestCase]
+        public void foglaltTesztOutOfRange()
+        {
+            var test = new Eloadas(2, 2);
+            Assert.Throws<IndexOutOfRangeException>(() => {
+                test.Foglalt(10, 10);
+            });
+        }
+
+        [TestCase]
+        public void hibasFoglaltTeszt()
+        {
+            var test = new Eloadas(2, 2);
+            test.Lefoglal();
+            Assert.Throws<ArgumentException>(() => {
+                test.Foglalt(0, 0);
+            });
+        }
+    }
+}
